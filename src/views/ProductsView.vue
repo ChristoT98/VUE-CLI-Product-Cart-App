06dcd79e7df1e3ebd91e6e5a85dfe31b73fa4c4a
ChristoT98 <christolakshan@gmail.com>
2022-04-05 18:07:15 +0530

    finished

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="product">
+    <main class="wrapper">
+      <h1>Products</h1>
+
+      <div class="card-container">
+        <ProductCard v-for="(product, index) in inventory.slice(0,6)" :key="product.id" :index="index" :product="product" :addToCart="addToCart" class="card" />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import ProductCard from '@/components/ProductCardComponent.vue'
+export default {
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCard
+  }
+}
+</script>
